bc6903dcb719ffe50bb2a755065acefffe8606fd
Ignasbr <ignas.bruzas@gmail.com>
2018-02-20 18:49:20 +0200

    all new files

---
+++ pamok3/ikisimto.php
@@ -0,0 +1,11 @@
+<?php
+
+$skaicius = 1;
+
+$simtas = 100;
+
+for ($i = 1; $i <= $simtas; $i++ ){
+    $sum += $i;
+};
+
+echo $sum;
---
+++ pamok3/vartotojas.php
@@ -0,0 +1,31 @@
+<?php
+
+require('functions.php');
+
+//$nameJ = 'Jonas';
+//
+//$surnameJ = 'Jonaitis';
+//
+//pasisveikinam($nameJ , $surnameJ);
+
+//$jonas = [3, 6 ,1, 3];
+//
+//echo MasyvoVidurkis($jonas);
+
+//$zmones = [ ['vardas'=> 'Jonas', 'pavarde'=> 'Jonatis', 'email'=> 'jono@pastas.lt'],
+//            ['vardas'=> 'Tomas', 'pavarde'=> 'Tomaitis', 'email'=> 'tomo@pastas.lt'],
+//            ['vardas'=> 'Karolis', 'pavarde'=> 'Karolaitis', 'email'=> 'karolio@pastas.lt'],
+//            ['vardas'=> 'Labas', 'pavarde'=> 'As', 'email'=> 'krabas@pastas.lt']];
+//
+//echo info($zmones);
+
+//$zmones = [ ['vardas'=> 'Jonas', 'pavarde'=> 'Jonatis', 'email'=> 'jono@pastas.lt'],
+//           ['vardas'=> 'Tomas', 'pavarde'=> 'Tomaitis', 'email'=> 'tomo@pastas.lt'],
+//            ['vardas'=> 'Karolis', 'pavarde'=> 'Karolaitis', 'email'=> 'karolio@pastas.lt'],
+//            ['vardas'=> 'Labas', 'pavarde'=> 'Labutis', 'email'=> 'krabas@pastas.lt']];
+//
+//echo pasisveikinamInfo($zmones);
+
+$n = 5;
+
+echo faktorialas($n);
